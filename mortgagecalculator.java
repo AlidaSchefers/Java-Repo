c749fed790a0d0fef4f86ef41639ee0f14448a37
AlidaSchefers <alidadschefers@gmail.com>
2021-02-24 15:47:17 -0500

    added checks for input range and separated code into methods

--- mortgagecalculator.java
+++ mortgagecalculator.java
@@ -6,27 +6,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-        //user input: Principal, Annual Interest Rate (e.g. 3.92), Period (Years)
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        double annualInterestRate = scanner.nextDouble();
-        System.out.print("Period (Years): ");
-        int periodYears = scanner.nextInt();
+        int principal = (int)readNumber("Principal ($1K - $1M): ", 1000, 1_000_000);
+        float yearlyInterest = (float)readNumber("Annual Interest Rate: ", 1, 30);
+        byte periodYears = (byte)readNumber("Period (Years): ", 1, 30);
 
-        double monthlyInterestRatePercent = (annualInterestRate / PERCENT) / MONTHS_IN_YEAR;
-        int periodMonths = periodYears * MONTHS_IN_YEAR;
-
-        //there is implicit casting: periodMonths and principal become doubles in the math
-        double mortgage = principal * (
-                (monthlyInterestRatePercent * Math.pow(1+monthlyInterestRatePercent, periodMonths))
-                / ( Math.pow(1+monthlyInterestRatePercent, periodMonths) - 1 )
-                );
+        double mortgage = calculateMortgage(principal, yearlyInterest, periodYears);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.print(mortgageFormatted);
     }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextInt();
+            if (value >= min || value <= max)
+                break;
+            System.out.println("Enter a number between " + min + " and " + max + ".");
+        }
+        return value;
+    };
+
+    public static double calculateMortgage(int principal, double yearlyInterest, byte periodYears) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        double monthlyInterestPercent = (yearlyInterest / PERCENT) / MONTHS_IN_YEAR;
+        short periodMonths = (short)(periodYears * MONTHS_IN_YEAR);
+
+        double mortgage = principal * (
+                (monthlyInterestPercent * Math.pow(1 + monthlyInterestPercent, periodMonths))
+                        / (Math.pow(1 + monthlyInterestPercent, periodMonths) - 1)
+        );
+        return mortgage;
+    };
 }
